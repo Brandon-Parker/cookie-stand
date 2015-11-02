c09d3f84de5e3e7fd6a61a68d08545abdfe0372f
BRANDON PARKER <Brandon@dkparker.com>
2015-11-02 14:37:48 -0800

    Added in Random number generators for Customer Numbers, Hourly Sales, and Avg per Hour.

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+
 var pikePlace = {
+  store: 'Pike Place',
   minCustHr: 17,
   maxCustHr: 88,
   avgPerCust: 5.2,
+  hourlySales: [],
 
 calcRandCust: function() {
   return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
   },
-
 calcHourlySales: function() {
-  return this.avgPerCust + this.calcRandCust();
+  return Math.floor(this.avgPerCust * this.calcRandCust());
   },
 };
+for (var i = 0; i < hours.length; i++) {
+  pikePlace.hourlySales.push(pikePlace.calcHourlySales())
+}
+
 
 console.log(pikePlace.calcRandCust(pikePlace.minCustHr, pikePlace.maxCustHr));
