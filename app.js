951416267b5da6efaa9e0da50996ef1efa3733da
BRANDON PARKER <Brandon@dkparker.com>
2015-11-02 17:32:48 -0800

    fixed bug not totally out the Daily Total, and fixed the HTML for the JS

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
-
 var pikePlace = {
   store: 'Pike Place',
   minCustHr: 17,
@@ -14,30 +13,34 @@ var pikePlace = {
   calcHourlySales: function() {
     for (var i = 0; i < hours.length; i++) {
       var hrly = Math.floor(this.avgPerCust * this.calcRandCust());
+      console.log('hourly total is ' + hrly);
       this.hourlySales.push(hrly)
       this.dailyTotal += hrly;
+      console.log('running total is' + this.dailyTotal);
     }
   },
   makeList: function() {
     this.calcHourlySales();
-    var ulElem = document.createElement('ul');
+
     for (var i = 0; i < hours.length; i++) {
       var li = document.createElement('li');
       li.textContent = hours[i] + ': ' + this.hourlySales[i];
-      ulElem.appendChild(li);
+      pikeUl.appendChild(li);
     }
-    document.body.appendChild(ulElem);
+    var li = document.createElement('li');
+      li.textContent = 'Daily Total: ' + this.dailyTotal;
+      pikeUl.appendChild(li);
     //Create an element
     //Add content to the element
     //append the element to the DOM
-      // li.textContent = hrly[i] + ': ' + this.dailyTotal[i];
-      // ulElem.appendChild(li);
+
 
   }
 };
 
-pikePlace.calcHourlySales()
-console.log(pikePlace.hourlySales)
+var pikeUl = document.getElementById('pike');
+pikePlace.makeList()
+// console.log(pikePlace.hourlySales)
 
 // for (var i >= 0; i < pikePlace.hourlySales.length; i++) {
 //   0[i]
