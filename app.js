bad249ec11d79f54b84d6363d295b545eeb5afb4
lorentinbuca <lorentinb@hotmail.com>
2015-11-03 16:04:49 -0800

    Bug fixed, table showing

--- app.js
+++ app.js
@@ -47,160 +47,17 @@ function CookieStand(storeLocations, minCustHr, maxCustHr, avgPerCust) {
 };
 
 var pikePlace = new CookieStand("Pike Place Market", 17, 88, 5.2);
+var seaTac = new CookieStand("SeaTac Airport", 6, 44, 1.2);
+var southCenter = new CookieStand("South Center Mall", 11, 38, 1.9);
+var bellevueSquare = new CookieStand("Bellevue Square Mall", 20, 48, 3.3);
+var alki = new CookieStand("Alki Beach", 3, 24, 2.6);
+
 var tbl = document.createElement('table');
 pikePlace.displayData();
-
-
+seaTac.displayData();
+southCenter.displayData();
+bellevueSquare.displayData();
+alki.displayData();
 document.body.appendChild(tbl);
 
 
-
-
-/*
-var seaTac = {
-  store: 'SeaTac Airport',
-  minCustHr: 6,
-  maxCustHr: 44,
-  avgPerCust: 1.2,
-  hourlySales: [],
-  dailyTotal: 0,
-
-  calcRandCust: function() {
-    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
-  },
-  calcHourlySales: function() {
-    for (var i = 0; i < hours.length; i++) {
-      var hrly = Math.floor(this.avgPerCust * this.calcRandCust());
-      console.log('hourly total is ' + hrly);
-      this.hourlySales.push(hrly)
-      this.dailyTotal += hrly;
-      console.log('running total is ' + this.dailyTotal);
-    }
-  },
-  makeList: function() {
-    this.calcHourlySales();
-
-    for (var i = 0; i < hours.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = hours[i] + ': ' + this.hourlySales[i];
-      seatacUl.appendChild(li);
-    }
-    var li = document.createElement('li');
-      li.textContent = 'Daily Total: ' + this.dailyTotal;
-      seatacUl.appendChild(li);
-    }
-  };
-
-var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
-var southCenter = {
-  store: 'South Center Mall',
-  minCustHr: 11,
-  maxCustHr: 38,
-  avgPerCust: 1.9,
-  hourlySales: [],
-  dailyTotal: 0,
-
-  calcRandCust: function() {
-    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
-  },
-  calcHourlySales: function() {
-    for (var i = 0; i < hours.length; i++) {
-      var hrly = Math.floor(this.avgPerCust * this.calcRandCust());
-      console.log('hourly total is ' + hrly);
-      this.hourlySales.push(hrly)
-      this.dailyTotal += hrly;
-      console.log('running total is ' + this.dailyTotal);
-    }
-  },
-  makeList: function() {
-    this.calcHourlySales();
-
-    for (var i = 0; i < hours.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = hours[i] + ': ' + this.hourlySales[i];
-      southCenterUl.appendChild(li);
-    }
-    var li = document.createElement('li');
-      li.textContent = 'Daily Total: ' + this.dailyTotal;
-      southCenterUl.appendChild(li);
-    }
-  };
-
-var bellevueSquare = {
-  store: 'Bellevue Square Mall',
-  minCustHr: 11,
-  maxCustHr: 38,
-  avgPerCust: 1.9,
-  hourlySales: [],
-  dailyTotal: 0,
-
-  calcRandCust: function() {
-    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
-  },
-  calcHourlySales: function() {
-    for (var i = 0; i < hours.length; i++) {
-      var hrly = Math.floor(this.avgPerCust * this.calcRandCust());
-      console.log('hourly total is ' + hrly);
-      this.hourlySales.push(hrly)
-      this.dailyTotal += hrly;
-      console.log('running total is ' + this.dailyTotal);
-    }
-  },
-  makeList: function() {
-    this.calcHourlySales();
-
-    for (var i = 0; i < hours.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = hours[i] + ': ' + this.hourlySales[i];
-      bellevueSquareUl.appendChild(li);
-    }
-    var li = document.createElement('li');
-      li.textContent = 'Daily Total: ' + this.dailyTotal;
-      bellevueSquareUl.appendChild(li);
-    }
-  };
-
-var alki = {
-  store: 'Alki',
-  minCustHr: 11,
-  maxCustHr: 38,
-  avgPerCust: 1.9,
-  hourlySales: [],
-  dailyTotal: 0,
-
-  calcRandCust: function() {
-    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
-  },
-  calcHourlySales: function() {
-    for (var i = 0; i < hours.length; i++) {
-      var hrly = Math.floor(this.avgPerCust * this.calcRandCust());
-      console.log('hourly total is ' + hrly);
-      this.hourlySales.push(hrly)
-      this.dailyTotal += hrly;
-      console.log('running total is ' + this.dailyTotal);
-    }
-  },
-  makeList: function() {
-    this.calcHourlySales();
-
-    for (var i = 0; i < hours.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = hours[i] + ': ' + this.hourlySales[i];
-      alkiUl.appendChild(li);
-    }
-    var li = document.createElement('li');
-      li.textContent = 'Daily Total: ' + this.dailyTotal;
-      alkiUl.appendChild(li);
-    }
-  };*/
-
-/*var pikeUl = document.getElementById('pike');*/
-/*var seatacUl = document.getElementById('seatac');
-var southCenterUl = document.getElementById('southCenter');
-var bellevueSquareUl = document.getElementById('bellevueSquare');
-var alkiUl = document.getElementById('alki');*/
-/*pikePlace.makeList()*/
-/*seaTac.makeList()
-southCenter.makeList()
-alki.makeList()
-bellevueSquare.makeList()*/
