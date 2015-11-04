e99eb37c79cb31c4b5ae5058aec27326418ebd32
BRANDON PARKER <Brandon@dkparker.com>
2015-11-04 11:08:36 -0800

    fixed constructor by getting rid of extra commands that were not needed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+var locations = [];
 function CookieStand(storeLocations, minCustHr, maxCustHr, avgPerCust) {
   this.storeLocations = storeLocations;
   this.minCustHr = minCustHr;
@@ -6,6 +7,7 @@ function CookieStand(storeLocations, minCustHr, maxCustHr, avgPerCust) {
   this.avgPerCust = avgPerCust;
   this.hourlySales = [];
   this.dailyTotal = 0;
+  locations.push(this);
 
   this.custCalc = function() {
     return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
@@ -20,16 +22,6 @@ function CookieStand(storeLocations, minCustHr, maxCustHr, avgPerCust) {
 
   this.displayData = function() {
     this.calcHourlySales();
-    var heading = document.createElement('tr');
-    var blank = document.createElement('td');
-    blank.textContent = '';
-    heading.appendChild(blank);
-
-    for(var i = 0; i < hours.length; i++) {
-      var storeHours = document.createElement('td');
-      storeHours.textContent = hours[i];
-      heading.appendChild(storeHours);
-    };
 
     var row = document.createElement('tr');
     var location = document.createElement('th');
@@ -54,6 +46,9 @@ var seaTac = new CookieStand("SeaTac Airport", 6, 44, 1.2);
 var southCenter = new CookieStand("South Center Mall", 11, 38, 1.9);
 var bellevueSquare = new CookieStand("Bellevue Square Mall", 20, 48, 3.3);
 var alki = new CookieStand("Alki Beach", 3, 24, 2.6);
+var issaquah = new CookieStand("The Quah", 6, 35, 7.7);
+var junction = new CookieStand("Alaska Junction", 1, 4, 2.6);
+var enumclaw = new CookieStand("Enumclaw", 15, 21, 10.6);
 
 var tbl = document.createElement('table');
 var headerRow = document.createElement('thead');
@@ -68,11 +63,20 @@ for (var i = 0; i < hours.length; i++) {
   var dailyTotal = document.createElement('th');
   dailyTotal.textContent = "Total";
   headerRow.appendChild(dailyTotal);
+  tbl.appendChild(headerRow);
 
-tbl.appendChild(headerRow);
-pikePlace.displayData();
-seaTac.displayData();
-southCenter.displayData();
-bellevueSquare.displayData();
-alki.displayData();
-document.body.appendChild(tbl);
+function displayAllLocations(){
+for (var i = 0; i < locations.length; i++){
+  locations[i].displayData();
+  }
+  document.body.appendChild(tbl);
+}
+
+displayAllLocations();
+
+// function updateLocation(){
+//   pikePlace.avgPerCust = 0;
+//   displayAllLocations();
+// }
+
+// updateLocation();
